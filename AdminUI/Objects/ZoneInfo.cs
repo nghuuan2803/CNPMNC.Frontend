d3cf002079f6ca8b253297cb01345cc7472b4bad
huuan28 <nghuuan2803@gmail.com>
2024-09-22 13:04:59 +0700

    Add project files.

---
+++ AdminUI/Objects/ZoneInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminUI.Objects
+{
+    public class ZoneInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Percent { get; set; }
+    }
+}
